74da1d4eabd4a94f7d8eb64fc784cab4ca7aa1b5
yyboom <yoonyoung37@naver.com>
2022-10-11 11:33:48 +0900

    custdto-choyunyoung

--- furnishop/src/main/java/com/furni/dto/CustDTO.java
+++ furnishop/src/main/java/com/furni/dto/CustDTO.java
@@ -1,5 +1,27 @@
 package com.furni.dto;
 
-public class CustDTO {
+import java.util.Date;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class CustDTO {
+	private String custid;
+	private String custpwd;
+	private String custname;
+	private String custaddr;
+	private String custtel;
+	private String custmail;
+	private Date custdate;
+	private String withdraw;
+	private int custbirth;
+	
 }
