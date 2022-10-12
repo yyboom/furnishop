76b3626e61ae5d3e6b8bd2c4a1efa1216710849d
yyboom <yoonyoung37@naver.com>
2022-10-12 13:21:29 +0900

    orderpage부분 수정 완료-choyunyoung

--- furnishop/src/test/java/com/furni/orderpage/InsertOrderpage.java
+++ furnishop/src/test/java/com/furni/orderpage/InsertOrderpage.java
@@ -22,11 +22,15 @@ class InsertOrderpage {
 	@Test
 	void contextLoads() {
 		OrderpageDTO order = new OrderpageDTO(0,"id02",new Date("Thu, 17 Aug 2022 00:00:00 +900"),"네이버페이", 1,"Y", "할머니댁","서울특별시 영등포구","남지현","010-2345-8746", "침대", 0);
-		try {
-	           int r = service.register1(order);
-	           System.out.println("Register OK"+order.getOrderno());
-	           DetailorderDTO detail = new DetailorderDTO(0,r, 100, "침대", "베이지", 2, "bed.jpg", "ida02");
+		try {  
+			//int r = service.register1(order); //-> 1로 들어감
+	           service.register(order);
+	           int r = order.getOrderno();
+	           System.out.println("Register OK"+r);
+	           DetailorderDTO detail = new DetailorderDTO(0,r, 100, "침대", "베이지", 2, "bed.jpg", "id02");
+	           DetailorderDTO detail1 = new DetailorderDTO(0,r, 101, "옷장", "그레이", 2, "closet.jpg", "id02");
 	           service1.register(detail);
+	           service1.register(detail1);
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
