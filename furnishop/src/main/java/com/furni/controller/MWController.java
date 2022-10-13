ef6ba2478c8e5dc4bbc158d07ee8833b3e7a1bb3
manex <manex@minwoo>
2022-10-14 02:29:13 +0900

    update

---
+++ furnishop/src/main/java/com/furni/controller/MWController.java
@@ -0,0 +1,55 @@
+package com.furni.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.furni.dto.CartDTO;
+import com.furni.service.CartService;
+
+@Controller
+@RequestMapping("/cart")
+public class MWController {
+
+	String dir = "cart/";
+	
+	@Autowired
+	CartService service;
+	
+	@RequestMapping("/get")
+	public String get(Model model,String id) {
+		List<CartDTO> list = null;
+		try {
+			list = service.cartall(id);
+			model.addAttribute("list",list);
+			model.addAttribute("center",dir+"get");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		System.out.println(list);
+		return "main";
+	}
+	
+	@RequestMapping("/delete")
+	public String delete(Model model, Integer cartno,String custid) {
+		try {
+			service.remove(cartno);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return "redirect:cart?id="+ custid;
+	}
+}
+
+
+
+
+
+
+
+
+
+
