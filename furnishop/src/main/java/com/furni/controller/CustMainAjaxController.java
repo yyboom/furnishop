6bfcfe176d182332c4ead10857b7e19c6cb5706d
ChoJae-won <jwcho1996@gmail.com>
2022-10-14 17:47:04 +0900

    commit

--- furnishop/src/main/java/com/furni/controller/CustMainAjaxController.java
+++ furnishop/src/main/java/com/furni/controller/CustMainAjaxController.java
@@ -1,5 +1,6 @@
 package com.furni.controller;
 
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
