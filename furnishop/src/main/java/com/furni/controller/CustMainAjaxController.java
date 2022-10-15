3240d70c6d3047e21e0e99dae46522f3c68f2f4a
yyboom <yoonyoung37@naver.com>
2022-10-15 11:28:56 +0900

    main수정-choyunyoung

--- furnishop/src/main/java/com/furni/controller/CustMainAjaxController.java
+++ furnishop/src/main/java/com/furni/controller/CustMainAjaxController.java
@@ -2,7 +2,6 @@ package com.furni.controller;
 
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -44,7 +43,6 @@ public class CustMainAjaxController {
 	public Object addwish(WishlistDTO wish) {
 		try {
 			service.register(wish);
-			System.out.println(wish);
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
